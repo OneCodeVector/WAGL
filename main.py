d8d81287caf717d03dc3b6157d1bd4a589bc3cc2
OneCodeVector <117361107+OneCodeVector@users.noreply.github.com>
2022-11-11 01:34:55 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import wagl
+import sys
+import  time
+
+Keyboard = wagl.Drivers.Keyboard
+Mouse = wagl.Drivers.Mouse()
+Window = wagl.Drivers.Graphics.Window()
+
+Mouse_Position = None
+Running = True
+
+Keyboard.Get_Key_Events.start()
+
+while Running:
+    Window.Load_Frame()
+
+    Old_Mouse_Position = Mouse.position
+    Mouse = wagl.Drivers.Mouse()
+    New_Mouse_Position = Mouse.position
+
+    if Old_Mouse_Position != New_Mouse_Position:
+        Mouse_Position = New_Mouse_Position
+
+    if Keyboard.Key_Pressed != None:
+        if str(Keyboard.Key_Pressed) == "Key.esc":
+            Running = False
+            sys.exit()
+
+        Keyboard.Key_Pressed = None
+    time.sleep(0.01)
+
+Keyboard.Get_Key_Events.join()
