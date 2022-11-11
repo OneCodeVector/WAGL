27b9265896145e28dbab0d025bbea74e3e3613a4
OneCodeVector <117361107+OneCodeVector@users.noreply.github.com>
2022-11-11 13:31:48 -0500

    Window Delete
    Added Window Deletion. Also added easier args for customizing the window when created.

--- wagl/Drivers/Graphics.py
+++ wagl/Drivers/Graphics.py
@@ -15,16 +15,26 @@ from time import sleep
 
 class Window:
     def __init__(self, **kwargs):
+        #data
+        self.title = None
+        self.geometry = (900, 900)
+        
+        #window
         self.wc = win32gui.WNDCLASS()
         self.wc.lpszClassName = 'test_win32gui_1'
         self.wc.style = win32con.CS_GLOBALCLASS | win32con.CS_VREDRAW | win32con.CS_HREDRAW
         self.wc.hbrBackground = win32con.COLOR_WINDOW + 1
         self.class_atom = win32gui.RegisterClass(self.wc)
-        self.win_obj = win32gui.CreateWindow(self.wc.lpszClassName,
-                                             'First window EVER made in WAGL',
-                                             win32con.WS_CAPTION | win32con.WS_VISIBLE,
-                                             100, 100, 900, 900, 0, 0, 0, None)
+        self.win_obj = win32gui.CreateWindow(
+            self.wc.lpszClassName,
+            self.title,
+            win32con.WS_CAPTION | win32con.WS_VISIBLE,
+            100, 100, *self.geometry, 0, 0, 0, None
+        )
 
     def Load_Frame(self):
         win32gui.InvalidateRect(self.win_obj,None,False)
         win32gui.PumpWaitingMessages()
+        
+    def Destroy(self):
+        win32gui.PostMessage(self.wc, win32con.WM_CLOSE,0,0)
