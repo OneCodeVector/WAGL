4d74404c1cc74c3c2a514ae237e2ec12aaf72f50
OneCodeVector <117361107+OneCodeVector@users.noreply.github.com>
2022-11-14 20:37:03 -0500

    Update Sound.py

--- wagl/Drivers/Sound.py
+++ wagl/Drivers/Sound.py
@@ -7,12 +7,19 @@ except:
     system("python Scripts/pywin32_postinstall.py -install")
     import win32api
 
+from threading import *
+
+def beep(freq, dela):
+    win32api.Beep(
+        freq,
+        dela,
+    )
+
 class Sound():
     def __init__(self, **kwargs):
         self.freq = 500
-        self.dela = 3000
+        self.dela = 1000
     def Play_Freq(self, freq, dela):
-        win32api.Beep(
-            freq,
-            dela,
-        )
+        Sound_Thread = Thread(target = beep, args = (self.freq, self.dela))
+        Sound_Thread.start()
+        Sound_Thread.join()
